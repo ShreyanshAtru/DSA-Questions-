35b25c93c7b90dc0af00fa7f01e143cb1382c614
ShreyanshAtru <79184646+ShreyanshAtru@users.noreply.github.com>
2025-01-29 11:52:20 +0530

    linkedlist (#2)
    Co-authored-by: Shreyansh Jain <shreyanshjain@Shreyanshs-MacBook-Air.local>

--- Array Questions/stockBuySell.py
+++ Array Questions/stockBuySell.py
@@ -1,9 +1,10 @@
 # Stock Buy and Sell – Multiple Transaction Allowed
-
+# https://www.geeksforgeeks.org/batch/gfg-160-problems/track/arrays-gfg-160/problem/stock-buy-and-sell2615
 """
 I need to check the first value and after that will be in increasing order,
 if not then just pervious value minus first will be profit and increment the first ptr to next value
 """
+
 arr = [100, 180, 260, 310, 40, 535, 695]
 first = 0
 second = 0
